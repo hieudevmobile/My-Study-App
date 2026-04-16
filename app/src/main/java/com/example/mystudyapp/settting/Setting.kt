e7a2cf5d00948d099f78f7dafef3b9099e00bc41
Cuongnguyen232-Vy <cuongnguyen23022k4@gmail.com>
2026-04-17 00:26:52 +0700

    [feat] Tích hợp logic module Tiện ích và Setting

--- app/src/main/java/com/example/mystudyapp/settting/Setting.kt
+++ app/src/main/java/com/example/mystudyapp/settting/Setting.kt
@@ -1,4 +1,33 @@
-package com.example.mystudyapp.settting
+package com.example.workandstudy_app.settting
 
-class Setting {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.workandstudy_app.R
+import com.example.workandstudy_app.databinding.SettingActitivityBinding
+import com.example.workandstudy_app.login.ScreenLogin
+
+class Setting : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: SettingActitivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SettingActitivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        binding.signOut.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.signOut ->{
+                intent=Intent(this, ScreenLogin::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
 }
